13cbe63eee53042112d8006a109aec82af524284
Alan <a49.piron@gmail.com>
2023-02-18 12:11:42 +0100

    J3

---
+++ TP/TP2/composants/liste_memes.js
@@ -0,0 +1,17 @@
+const liste_memes = {
+    template: `
+    
+    <h2>Voici {{ nb_memes }} meme{{ nb_memes > 1 ? 's' : '' }}</h2>
+    <slot name="button"></slot>
+    <section class="d-flex flex-wrap">
+        <div v-for="(meme,index) in liste_memes.slice(0,nb_memes)" class="card me-3 mt-2" style="width: 18rem;">
+            <img :src="meme.url" class="card-img-top" alt="...">
+            <div class="card-body">
+                <slot name="nom_meme" :nom_meme="meme.name" :numero_meme="index+1" ></slot>
+            </div>
+        </div>
+    </section>
+    
+    `,
+    props: ['liste_memes', 'nb_memes']
+}
